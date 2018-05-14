25d263d96670e577259efb3c55d73dc7d5f33f15
Kent Delante <kdelante14@gmail.com>
2018-05-14 18:45:50 +0800

    fixed local weather app api request

--- static/scripts/localWeather.js
+++ static/scripts/localWeather.js
@@ -1,11 +1,19 @@
 window.onload = function() {
+    
+    var apiUrl = "https://fcc-weather-api.glitch.me/api/current?"
 
     function success(pos) {
-
-        var apiUrl = 'https://fcc-weather-api.glitch.me/api/current?'
         
         var lat = pos.coords.latitude;
         var lon = pos.coords.longitude;
+        var apiRequest = new Request(`${apiUrl}lat=${lat}lon=${lon}`, {
+            method: "GET",
+            mode: "cors",
+            headers: new Headers({
+                'Content-Type': "text/plain"
+            })
+        });
+        
 
         fetch(`${apiUrl}lat=${lat}&lon=${lon}`)
             .then(function(response) {
@@ -13,14 +21,14 @@ window.onload = function() {
             }).then(function(response) {
 
                 var descriptionView = document
-                    .querySelector('.apps__localWeather__description');
+                    .querySelector(".apps__localWeather__description");
 
                 descriptionView.innerHTML = response
                     .weather[0].description.toString();
             });
         
         var weatherDescription = document
-            .querySelector('.apps__localWeather__description');
+            .querySelector(".apps__localWeather__description");
 
     }
 
