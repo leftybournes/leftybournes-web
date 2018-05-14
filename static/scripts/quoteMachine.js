249ef57c36415560f92027b2b6f4149e765bee05
Kent Delante <kdelante14@gmail.com>
2018-05-14 18:06:25 +0800

    Completed quote machine features

--- static/scripts/quoteMachine.js
+++ static/scripts/quoteMachine.js
@@ -1,27 +1,36 @@
 window.onload = function() {
     
     var apiUrl = "https://sw-said-who.herokuapp.com/api/random";
-        //"http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+    var tweetUrl = "https://twitter.com/intent/tweet?text="    
     var apiRequest = new Request(apiUrl, {
-        method: 'GET',
-        mode: 'cors',
+        method: "GET",
+        mode: "cors",
         headers: new Headers({
-            'Content-Type': 'text/plain'
+            'Content-Type': "text/plain"
         })
     });
 
-    getQuote();
+    newQuote();
     
-    var quoteField = document.querySelector(".apps__quoteMachine__quote");
-    var authorField = document.querySelector(".apps__quoteMachine__author");
-    var quoteButton = document.querySelector(".apps__quoteMachine__getQuote")
+    var quoteField = document
+        .querySelector(".apps__quoteMachine__quoteWrapper__quote");
+    var authorField = document
+        .querySelector(".apps__quoteMachine__quoteWrapper__author");
+    var quoteButton = document
+        .querySelector(".apps__quoteMachine__buttonWrapper__getQuote");
+    var tweetButton = document
+        .querySelector(".apps__quoteMachine__buttonWrapper__tweet");
 
-    quoteButton.addEventListener('click', getQuote);
+    quoteButton.addEventListener("click", newQuote);
 
-    function getQuote() {
+    function newQuote() {
         fetch(apiRequest)
             .then(function(response) {
                 return response.json();
+            })
+            .catch(function(error) {
+                quoteField.innerHTML = "Error fetching quote";
+                authorField.innerHTML = "Error fetching quote";
             })
             .then(function(response) {
                 var quote = response.quote;
@@ -29,6 +38,11 @@ window.onload = function() {
 
                 quoteField.innerHTML = quote;
                 authorField.innerHTML = `- ${author}`;
-        });
+
+
+                tweetButton.href = `${tweetUrl}${quote} - ${author}`;
+            });
     }
+
+
 };
